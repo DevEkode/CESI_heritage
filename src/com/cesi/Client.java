a779759a62301a2c38dcd71ea2981d5b93718e4b
stef-gitHub <41380342+stef-gitHub@users.noreply.github.com>
2020-07-02 14:45:43 +0200

    Classe Client

---
+++ src/com/cesi/Client.java
@@ -0,0 +1,32 @@
+package com.cesi;
+
+public class Client {
+
+    private long numero;
+    private String nom;
+
+    public Client(long numero, String nom) {
+        this.numero = numero;
+        this.nom = nom;
+    }
+
+    public long getNumero() {
+        return numero;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "numero=" + numero +
+                ", nom='" + nom + '\'' +
+                '}';
+    }
+}
--- src/com/cesi/Main.java
+++ src/com/cesi/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Client client1 = new Client(1,"Martin");
+        Client client2 = new Client(2,"Dupont");
+
+        System.out.println(client1);
+        System.out.println(client2);
+
+        client2.setNom("Pichon");
+        System.out.println(client2);
     }
 }
